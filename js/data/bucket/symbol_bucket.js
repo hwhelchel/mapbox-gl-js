293dc4c9b631dc1a87315667b7c99fd156a38bb9
xrwang <xiaowei@mapbox.com>
2016-08-03 10:56:38 -0700

    clear comments

--- js/data/bucket/symbol_bucket.js
+++ js/data/bucket/symbol_bucket.js
@@ -79,7 +79,7 @@ function addVertex(array, x, y, ox, oy, tx, ty, minzoom, maxzoom, labelminzoom, 
             // offset
             Math.round(ox * 64), // use 1/64 pixels for placement
             Math.round(oy * 64),
-            // data1
+            // data1, x y coordinates of where to find symbol on texture
             tx / 4,                   // tex
             ty / 4,                   // tex
             // data2
